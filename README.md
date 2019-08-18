84c4dbf3d493ce593fef6e6aa7d81207d5f16573
ArashMidos <arshiamidos@gmail.com>
2019-08-19 01:37:41 +0430

    website comlte

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Asteroid 👾☄️👨🏻‍🚀🚀   
 it's an idea from @mojombo ![asteroids](https://github.com/mojombo/asteroids)
+and @erkie ![erkie](https://github.com/erkie/erkie.github.com)  
+## TODO
+[ ] - ![link](https://github.com/microsoft/vscode/pull/66418)
+[ ] - convert images to subimage svg
--- src/extension.ts
+++ src/extension.ts
@@ -7,151 +7,132 @@ import fetch from 'node-fetch';
 let panel: vscode.WebviewPanel | undefined = undefined;
 
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('extension.asteroid', async () => {
-        if(!vscode.window.activeTextEditor){
-          vscode.window.showErrorMessage('open a source file and use in active editor please ⁉️ ');
-          return;
-        }
-        vscode.window.showInformationMessage('asteroid started 🚀!');
-
-        //let sourceCode=vscode.window.activeTextEditor.document.getText();
-
-       let sourceCode=await fetch("https://www.managroup.co/index.html").then(r=>{
-        let s=r.text() ; 
-       console.log(s);
-         return s;
-         
-        });
-        sourceCode=sourceCode.replace(/\.\/static/g,"https://www.managroup.co/static")
-        console.log(">>>",sourceCode);
-        
-
-         panel = vscode.window.createWebviewPanel(
-            'ArashMidos',
-            'Asteroid Game 🚀',
-            vscode.ViewColumn.One,
-            {
-                enableScripts: true
-            }
-          );
-
-          const onDiskPath = vscode.Uri.file(
-            path.join(context.extensionPath, 'utils', 'asteroids.js')
-          );
-
-          panel.webview.html = getWebviewContent(
-            onDiskPath.with({ scheme: 'vscode-resource' }),
-            sourceCode
-            );
-            
-          panel.webview.onDidReceiveMessage(
-            message => {
-              switch (message.command) {
-                case 'alert':
-                  vscode.window.showErrorMessage(message.text);
-                  panel!.dispose();
-                  return;
-              }
-            },
-            undefined,
-            context.subscriptions
-          );
-          
-          panel.onDidDispose(
-            () => {
-            },
-            null,
-            context.subscriptions
-          );
-          //currentPanel.webview.postMessage({ command: 'refactor' });
-  });
-  
-  let disposable2 = vscode.commands.registerCommand('extension.asteroid_web', async () => {
-    
-
+  let disposable = vscode.commands.registerCommand('extension.asteroid', async () => {
+    if (!vscode.window.activeTextEditor) {
+      vscode.window.showErrorMessage('open a source file and use in active editor please ⁉️ ');
+      return;
+    }
     vscode.window.showInformationMessage('asteroid started 🚀!');
-    let which =await vscode.window.showInputBox({placeHolder:"which website ?(ex https://arshiamidos.github.io)"});
-    if(!which || which!.trim()!.length===0){
+
+    panel = vscode.window.createWebviewPanel(
+      'ArashMidos',
+      'Asteroid Game 🚀',
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true
+      }
+    );
+
+    const onDiskPath = vscode.Uri.file(
+      path.join(context.extensionPath, 'utils', 'asteroids.js')
+    );
+
+    panel.webview.html = getWebviewContent(
+      onDiskPath.with({ scheme: 'vscode-resource' }),
+      vscode.window.activeTextEditor.document.getText()
+    );
+
+    panel.webview.onDidReceiveMessage(
+      message => {
+        switch (message.command) {
+          case 'alert':
+            vscode.window.showErrorMessage(message.text);
+            panel!.dispose();
+            return;
+        }
+      },
+      undefined,
+      context.subscriptions
+    );
+
+    panel.onDidDispose(
+      () => {
+      },
+      null,
+      context.subscriptions
+    );
+    //currentPanel.webview.postMessage({ command: 'refactor' });
+  });
+
+  let disposable2 = vscode.commands.registerCommand('extension.asteroid_web', async () => {
+
+    let which = await vscode.window.showInputBox({ placeHolder: "which website ?(ex https://arshiamidos.github.io)" });
+    if (!which || which!.trim()!.length === 0) {
       vscode.window.showErrorMessage("please tell me your target 😇");
       return;
-
     }
-    //let sourceCode=vscode.window.activeTextEditor.document.getText();
+    vscode.window.showInformationMessage('asteroid started 🚀!');
 
-   let sourceCode=await fetch(which).then(r=> r.text());
-    
-     panel = vscode.window.createWebviewPanel(
-        'ArashMidos',
-        'Asteroid Game 🚀',
-        vscode.ViewColumn.One,
-        {
-            enableScripts: true
+    let sourceCode = await fetch(which).then(r => r.text());
+
+    panel = vscode.window.createWebviewPanel(
+      'ArashMidos',
+      `Asteroid Game [${which}]🚀`,
+      vscode.ViewColumn.One,
+      {
+        enableScripts: true
+      }
+    );
+
+    const onDiskPath = vscode.Uri.file(
+      path.join(context.extensionPath, 'utils', 'asteroids.js')
+    );
+
+    panel.webview.html = getWebSite(
+      onDiskPath.with({ scheme: 'vscode-resource' }),
+      sourceCode,
+      which);
+
+    panel.webview.onDidReceiveMessage(
+      message => {
+        switch (message.command) {
+          case 'alert':
+            vscode.window.showWarningMessage(message.text);
+            panel!.dispose();
+            return;
         }
-      );
+      },
+      undefined,
+      context.subscriptions
+    );
 
-      const onDiskPath = vscode.Uri.file(
-        path.join(context.extensionPath, 'utils', 'asteroids.js')
-      );
-
-      panel.webview.html = getWebSite(
-        onDiskPath.with({ scheme: 'vscode-resource' }),
-        sourceCode,
-        which
-        );
-        
-      panel.webview.onDidReceiveMessage(
-        message => {
-          switch (message.command) {
-            case 'alert':
-              vscode.window.showWarningMessage(message.text);
-              panel!.dispose();
-              return;
-          }
-        },
-        undefined,
-        context.subscriptions
-      );
-      
-      panel.onDidDispose(
-        () => {
-        },
-        null,
-        context.subscriptions
-      );
-      //currentPanel.webview.postMessage({ command: 'refactor' });
-});
-	context.subscriptions.push(disposable);
-	context.subscriptions.push(disposable2);
+    panel.onDidDispose(
+      () => { },
+      null,
+      context.subscriptions
+    );
+  });
+  context.subscriptions.push(disposable);
+  context.subscriptions.push(disposable2);
 }
 
-export function deactivate() {}
-function getWebSite(jsPath:vscode.Uri,sourceCode:string,which:string) {
-  
-  sourceCode=sourceCode.replace(/src=\"\.\//g,`src="${which}/`);
-  sourceCode=sourceCode.replace(/src=\'\.\//g,`src='${which}/`);
+export function deactivate() { }
+function getWebSite(jsPath: vscode.Uri, sourceCode: string, which: string) {
 
-  sourceCode=sourceCode.replace(/src=\"\.\.\//g,`src="${which}/`);
-  sourceCode=sourceCode.replace(/src=\'\.\.\//g,`src='${which}/`);
-  
-  sourceCode=sourceCode.replace(/src=\"(?!http)/g,`src="${which}/`);
-  sourceCode=sourceCode.replace(/src=\'(?!http)/g,`src='${which}/`);
-  
-  sourceCode=sourceCode.replace(/href=\"(?!http)/g,`href="${which}/`);
-  sourceCode=sourceCode.replace(/href=\'(?!http)/g,`href='${which}/`);
-  
-  console.log(sourceCode.substr(0,1000));
-  
-  sourceCode=sourceCode.replace("</html>",`
+  sourceCode = sourceCode.replace(/src=\"\.\//g, `src="${which}/`);
+  sourceCode = sourceCode.replace(/src=\'\.\//g, `src='${which}/`);
+
+  sourceCode = sourceCode.replace(/src=\"\.\.\//g, `src="${which}/`);
+  sourceCode = sourceCode.replace(/src=\'\.\.\//g, `src='${which}/`);
+
+  sourceCode = sourceCode.replace(/src=\"(?!http)/g, `src="${which}/`);
+  sourceCode = sourceCode.replace(/src=\'(?!http)/g, `src='${which}/`);
+
+  sourceCode = sourceCode.replace(/href=\"(?!http)/g, `href="${which}/`);
+  sourceCode = sourceCode.replace(/href=\'(?!http)/g, `href='${which}/`);
+
+
+  sourceCode = sourceCode.replace("</html>", `
     ${sourceCode}
     <script type="text/javascript" src="${jsPath}"></script>
     </html>`);
-  
+
   return sourceCode;
-  
-  
+
+
 }
-function getWebviewContent(jsPath:vscode.Uri,sourceCode:string) {
-    return `
+function getWebviewContent(jsPath: vscode.Uri, sourceCode: string) {
+  return `
     <DOCTYPE! html>
     <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
@@ -159,8 +140,26 @@ function getWebviewContent(jsPath:vscode.Uri,sourceCode:string) {
     </head>
     
     <body>
-    ${sourceCode}
+    ${(() => {
+      let rc = ["7", "8", "9", "a", "b", "c", "d", "e", "f"];
+      let x = sourceCode.split("\n").map(line => {
+
+        return `<div style="display:flex;flex-direction:row">` + line.split(" ").map(ch => {
+          if (!ch) {
+            return "&nbsp";
+          }
+          let r = Number(rc.length * Math.random()).toFixed(0);
+          let g = Number(rc.length * Math.random()).toFixed(0);
+          let b = Number(rc.length * Math.random()).toFixed(0);
+          return `&nbsp <p style="color:#${r + "" + g + b}">${ch}</p>`;
+        }).join(" ") + "</div>";
+
+      }).join("\n");
+
+      return x;
+
+    })()}
     </body>
     <script type="text/javascript" src="${jsPath}"></script>
     </html>`;
-  }
+}
